d245939b997437fc91a0afbf4c4cd9bba699fb1e
hututake2011 <stevenydt@gmail.com>
2023-08-22 14:27:35 -0700

    This is the first commit.

---
+++ Parameters.py
@@ -0,0 +1,36 @@
+# Parameters
+
+# DepotID
+DepotID = '4191'
+
+# Objective cost coefficient
+EVCoeff = 1
+GVCoeff = 2
+
+# Fixed cost
+fixCostEV = 200
+fixCostGV = 100
+
+# Range of vehicles
+EVRange = 100
+GVRange = 400
+
+# Number of vehicles
+EVNum = 2
+GVNum = 3
+
+# Number of orders
+HighDemand = 1500
+Demand2K = 2000
+Demand1K = 1000
+
+# System parameter
+# Distance between two drops within a location (mile)
+distance_between_drops = 1
+vehicle_speed_between_nodes = 45
+vehicle_speed_between_drops = 25
+maximum_working_time = 480   # Minutes
+service_time_per_drop = 1  # minutes
+EV_cost_per_mile = 0.25
+GV_cost_per_mile = 0.5
+labor_cost_per_minute = 0.45  # dollar per minute
